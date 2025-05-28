4c26518ebcf48abcd9510cdfdaff9f617f6c9275
Chris Dzombak <chris@dzombak.com>
2025-05-27 20:37:30 -0400

    fix playback bug

--- play/play.sh
+++ play/play.sh
@@ -5,7 +5,7 @@ cd /home/awos/audio
 
 while true; do
   cp latest.aiff play.aiff
-  aplay --device plughw:CARD=3,DEV=0 -f S16_BE -r 22050 /home/awos/audio/latest.aiff
+  aplay --device plughw:CARD=3,DEV=0 -f S16_BE -r 22050 /home/awos/audio/play.aiff
   rm play.aiff
   sleep 2
 done
